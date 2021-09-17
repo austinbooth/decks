b2130366b4c31186a37162d95ce787b45b9c926a
Austin Booth <austinb@thestartupfactory.tech>
2021-09-17 15:36:51 +0100

    adds vue config file with some custom settings

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+      // configure the workbox plugin
+      workboxPluginMode: 'GenerateSW',
+  
+      manifestOptions: {
+        name: "Decks-POC",
+        short_name: "Decks-POC",
+        background_color: "#12b0db",
+    }
+  }
+}
