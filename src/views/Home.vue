b35b15fb41b35cc094805c9dd0315e936ab43b43
Austin Booth <austinb@thestartupfactory.tech>
2021-09-17 12:49:13 +0100

    tidies a little

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,8 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
-    <div style="background: red">Drag me</div>
     <Counter />
-    <Card />
+    <!-- <Card /> -->
   </div>
 </template>
 
@@ -12,14 +11,14 @@
 import { defineComponent } from "vue";
 // import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import Counter from "@/components/Counter.vue"
-import Card from "@/components/Card.vue"
+// import Card from "@/components/Card.vue"
 
 export default defineComponent({
   name: "Home",
   components: {
     // HelloWorld,
     Counter,
-    Card
+    // Card
   },
 });
 </script>
