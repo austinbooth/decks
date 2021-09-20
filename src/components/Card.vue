8ff4b691fda532c55233d01aae5b71f8be909fb5
Austin Booth <austinb@thestartupfactory.tech>
2021-09-20 20:10:25 +0100

    adds logic to handle swipes/drags and not reply on detecting swipes with the vue touch handler library

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -40,16 +40,17 @@ export default defineComponent({
     return {
       cardData,
       isDragging,
+      startDragXcoord: -1
     };
   },
   methods: {
     swipeLeftHandler() {
-      console.log("<<< swiped LEFT!");
-      document.body.style.background = "red";
+      // console.log("<<< swiped LEFT!");
+      // document.body.style.background = "red";
     },
     swipeRightHandler() {
-      console.log(">>> swiped RIGHT!");
-      document.body.style.background = "green";
+      // console.log(">>> swiped RIGHT!");
+      // document.body.style.background = "green";
     },
     dragHandler(e: TouchEvent) {
       const windowWidth =
@@ -63,19 +64,36 @@ export default defineComponent({
           e.targetTouches[0].clientX - boundingRect.width / 2 + "px";
         this.block.style.top =
           e.targetTouches[0].clientY - boundingRect.height / 2 + "px";
-        console.log(this.block.getBoundingClientRect());
+        // console.log(this.block.getBoundingClientRect());
       }
     },
-    dragStartHandler() {
-      this.isDragging = true;
+    dragStartHandler(e: TouchEvent) {
+      this.isDragging = true
+      if (this.startDragXcoord === -1) {
+        this.startDragXcoord = e.changedTouches[0].clientX
+      }
+      // console.log('START:', e.changedTouches[0].clientX, e.changedTouches[0].clientX)
     },
     endHandler(e: TouchEvent) {
       if (this.block && this.isDragging) {
         this.block.style.left = "7.75%";
         this.block.style.top = "4.75%";
         this.isDragging = false;
-        console.log(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
-        // if later want to decide here if a card has been swiped or not then could use these values to compute how much a card has been moved across and in which direction
+
+        const delta = e.changedTouches[0].clientX - this.startDragXcoord
+        const screenWidth = e.view?.innerWidth
+        if (screenWidth) {
+          const deltaPc = delta / screenWidth
+          console.log(deltaPc)
+          if (deltaPc > 0.2) {
+            document.body.style.background = "green"
+          }
+
+          if (deltaPc < -0.2) {
+            document.body.style.background = "red"
+          }
+        }
+        console.log(delta)
       }
     },
   },
