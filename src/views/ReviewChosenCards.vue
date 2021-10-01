a456dcfb47398ffb66260aa6459b5b78e3d9aa1d
Austin Booth <austinb@thestartupfactory.tech>
2021-10-01 21:22:11 +0100

    displays swiped cards and makes clickable

--- src/views/ReviewChosenCards.vue
+++ src/views/ReviewChosenCards.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <p>Card review...</p>
+    <h3>You swiped right on:</h3>
+    <p
+      v-for="card in this.$store.state.cardsSwiped.filter(
+        (c) => c.swiped === 'right'
+      )"
+      :key="card.uid"
+      :id="card.uid"
+      @click="itemClick($event)"
+    >
+      {{ card.headline }}
+    </p>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  methods: {
+    itemClick(e: Event) {
+      const target = (e.currentTarget as HTMLInputElement)
+      console.log(target.id)
+    }
+  }
+})
+</script>
