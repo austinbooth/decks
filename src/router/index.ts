05329a1469ce083a27d62c167f2663b0665d7c2b
Austin Booth <austinb@thestartupfactory.tech>
2021-10-12 13:50:56 +0100

    reorganises to give a proper home page and deletes old unused components etc

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import Home from "../views/Home.vue"
 import ChooseChosenCard from "../views/ChooseChosenCard.vue"
+import Session from "../views/Session.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,13 +10,9 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/session",
+    name: "Session",
+    component: Session
   },
   {
     path: "/review",
@@ -27,6 +24,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
+
+// {
+//   path: "/about",
+//   name: "About",
+//   // route level code-splitting
+//   // this generates a separate chunk (about.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () =>
+//     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+// },
