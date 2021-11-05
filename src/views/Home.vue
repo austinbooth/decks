176db4bd655f7f1dcba639ca3a8552887ed12255
Austin Booth <austinb@thestartupfactory.tech>
2021-11-05 11:23:59 +0000

    creates buttons for each publisher deck

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,14 @@
   <div>
     <div class="home-container">
       <h3>Decks:</h3>
-      <button @click="this.$router.push('session')">View breakfast deck</button>
+      <button
+        v-for="deck in $data.publisherDecks"
+        :key="deck.uid"
+        class="deck-btn"
+        @click="this.$router.push('session')"
+      >
+        {{ deck.name }}
+      </button>
     </div>
     <ReviewCardsList />
   </div>
@@ -10,25 +17,29 @@
 
 <script lang="ts">
 import { defineComponent } from "vue"
-import ReviewCardsList from '@/components/ReviewCardsList.vue'
-import { openDB } from 'idb'
-import { createAnonymousUser, getAllPublisherDecks } from '@/firebase'
-import { DBNAME, USER_STORE_NAME, SWIPING_SESSIONS_STORE_NAME } from '@/indexeddb'
-import { DeckInfo } from '@/types'
+import ReviewCardsList from "@/components/ReviewCardsList.vue"
+import { openDB } from "idb"
+import { createAnonymousUser, getAllPublisherDecks } from "@/firebase"
+import {
+  DBNAME,
+  USER_STORE_NAME,
+  SWIPING_SESSIONS_STORE_NAME,
+} from "@/indexeddb"
+import { DeckInfo } from "@/types"
 
 (async () => {
   const version = 1
   const db = await openDB(DBNAME, version, {
-    upgrade: async(db, localDbVersion) => {
+    upgrade: async (db, localDbVersion) => {
       if (localDbVersion === 0) {
         await db.createObjectStore(USER_STORE_NAME)
         await db.createObjectStore(SWIPING_SESSIONS_STORE_NAME, {
-          keyPath: 'uid'
+          keyPath: "uid",
         })
         const createdUserId = await createAnonymousUser()
-        console.log('Created user:', createdUserId)
-      }      
-    }
+        console.log("Created user:", createdUserId)
+      }
+    },
   })
 })()
 
@@ -40,24 +51,31 @@ export default defineComponent({
   data() {
     const publisherDecks: DeckInfo[] = []
     return {
-      publisherDecks
+      publisherDecks,
     }
   },
   mounted() {
-    (async() => {
+    (async () => {
       const decks = await getAllPublisherDecks()
-      console.log('-->>', decks)
+      console.log("-->>", decks)
       this.$data.publisherDecks = decks ?? []
     })()
-  }
-});
+  },
+})
 </script>
 <style scoped>
-  .home-container {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    width: 90%;
-    margin: 18px auto 0;
-    padding: 8px 3px 24px;
-    border-radius: 5px;
-  }
+.home-container {
+  display: flex
+  flex-direction: column
+  align-items: center
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2)
+  width: 90%
+  margin: 18px auto 0
+  padding: 8px 3px 24px
+  border-radius: 5px
+}
+.deck-btn {
+  width: fit-content
+  margin: 4px 0
+}
 </style>
