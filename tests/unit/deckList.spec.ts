7e7cb1105f779ff0db145d6b2b25c84e90b6b248
Austin Booth <austinb@thestartupfactory.tech>
2021-11-24 23:20:41 +0000

    adds test

--- tests/unit/deckList.spec.ts
+++ tests/unit/deckList.spec.ts
@@ -22,4 +22,30 @@ describe('DeckList.vue', () => {
     expect(buttons[0].text()).toBe(data[0].name)
     expect(buttons[1].text()).toBe(data[1].name)    
   })
+  test('The store commit fn is called along with the router push method when one of the buttons is pressed', async () => {
+    const $store = {
+      state: {
+        chosenDeck: ''
+      },
+      commit: jest.fn()
+    }
+    const $router = {
+      push: jest.fn()
+    }
+    const wrapper = mount(DeckList, {
+      props: {
+        decks: data
+      },
+      global: {
+        mocks: {
+          $store,
+          $router
+        }
+      }
+    })
+    const buttons = wrapper.findAll('[class="deck-btn"]')
+    await buttons[0].trigger('click')
+    expect($store.commit).toHaveBeenCalled()
+    expect($router.push).toBeCalledWith('session')
+  })
 })
