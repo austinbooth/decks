cdf78c0cec69ef2565b1f32682980a86e2d5e70a
Austin Booth <austinb@thestartupfactory.tech>
2022-01-07 12:17:15 +0000

    adds checking of buttons

--- tests/e2e/specs/home.spec.ts
+++ tests/e2e/specs/home.spec.ts
@@ -1,8 +1,12 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Home", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h3", "Decks:");
-  });
+  it("Visits the home screen", () => {
+    cy.visit("/")
+    cy.contains("h3", "Decks:")
+    cy.get('button')
+      .should('have.length', 2)
+      .should('contain', 'Things to do')
+      .should('contain', 'Breakfast')
+  })
 });
